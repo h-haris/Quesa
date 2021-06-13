d1688fee6d76e7a07b2d97dd4ce089f9990b51b6
Ole Hartmann <h.haris@t-online.de>
2021-06-13 08:23:50 +0200

    merge jwwalker 4498cf6

--- Development/Source/Core/Support/E3Prefix.h
+++ Development/Source/Core/Support/E3Prefix.h
@@ -14,23 +14,23 @@
         For the current release of Quesa including 3D device support,
         please see: <https://github.com/h-haris/Quesa>
 
-        
+
         Redistribution and use in source and binary forms, with or without
         modification, are permitted provided that the following conditions
         are met:
-        
+
             o Redistributions of source code must retain the above copyright
               notice, this list of conditions and the following disclaimer.
-        
+
             o Redistributions in binary form must reproduce the above
               copyright notice, this list of conditions and the following
               disclaimer in the documentation and/or other materials provided
               with the distribution.
-        
+
             o Neither the name of Quesa nor the names of its contributors
               may be used to endorse or promote products derived from this
               software without specific prior written permission.
-        
+
         THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
         "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
         LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -209,5 +209,10 @@
 	#define		CATCH_ALL
 #endif
 
+#if QUESA_OS_WIN32
+	#define SAFE_STRCPY( dst, src, dstSize )	strcpy_s( dst, dstSize, src )
+#else
+	#define SAFE_STRCPY( dst, src, dstSize )	strlcpy( dst, src, dstSize )
+#endif
 
 #endif
